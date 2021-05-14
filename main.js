1dfe8ef1322d30f6da7e28e65ec0caedf85d696a
Titus Lim <titus.happy@gmail.com>
2021-05-14 19:48:06 +0800

    Changed a bunch of things
    I have changed the direction of this project. It now revolves around me self-learning javascript to create a command line-like startpage

---
+++ main.js
@@ -0,0 +1,27 @@
+function GetClock(){
+  var d=new Date();
+  var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+  if(nhour==0){ap=" AM";nhour=12;}
+  else if(nhour<12){ap=" AM";}
+  else if(nhour==12){ap=" PM";}
+  else if(nhour>12){ap=" PM";nhour-=12;}
+
+  if(nmin<=9) nmin="0"+nmin;
+
+  var clocktext=""+nhour+":"+nmin+ap+"";
+  document.getElementById('clockbox').innerHTML=clocktext;
+}
+
+function mainPrint(line){
+  //msg is meant for the final concat. string to be returned to the HTML file
+  //lineArr is for an array of words from the line string
+  var msg;
+  var lineArr = line.split("");
+  for (var i = 0; i < lineArr.length; i++) {
+    if (lineArr[i] == "ls") {
+      msg += lineArr[i].fontcolor("#39ff14");
+    }
+  }
+  let msg = "ls";
+  return msg;
+}

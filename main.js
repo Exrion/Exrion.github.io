30ce4f047dc79d9399b84d39208553b178602f97
Titus Lim <titus.happy@gmail.com>
2021-05-15 10:00:58 +0800

    Experimenting with JS
    My attempt to make a function that highlights the 'syntax' when entered

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+GetClock();
+setInterval(GetClock,1000);
+
+document.getElementById("consolePrint").innerHTML = mainPrint("test");
+
 function GetClock(){
   var d=new Date();
   var nhour=d.getHours(),nmin=d.getMinutes(),ap;
@@ -15,13 +20,15 @@ function GetClock(){
 function mainPrint(line){
   //msg is meant for the final concat. string to be returned to the HTML file
   //lineArr is for an array of words from the line string
+  var lineArr = [];
   var msg;
-  var lineArr = line.split("");
+
+  lineArr = line.split("");
   for (var i = 0; i < lineArr.length; i++) {
     if (lineArr[i] == "ls") {
       msg += lineArr[i].fontcolor("#39ff14");
     }
   }
-  let msg = "ls";
+  msg = "ls";
   return msg;
 }

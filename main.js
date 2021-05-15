39c4c1e2d42add41c56688f67ca2558de2bf498c
Titus Lim <titus.happy@gmail.com>
2021-05-15 22:13:16 +0800

    Cleaned up more code

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-GetClock();
-setInterval(GetClock,1000);
-
-document.getElementById("consolePrint").innerHTML = mainPrint("test");
-
-function GetClock(){
-  var d=new Date();
-  var nhour=d.getHours(),nmin=d.getMinutes(),ap;
-  if(nhour==0){ap=" AM";nhour=12;}
-  else if(nhour<12){ap=" AM";}
-  else if(nhour==12){ap=" PM";}
-  else if(nhour>12){ap=" PM";nhour-=12;}
-
-  if(nmin<=9) nmin="0"+nmin;
-
-  var clocktext=""+nhour+":"+nmin+ap+"";
-  document.getElementById('clockbox').innerHTML=clocktext;
-}
+document.getElementsByClassName('printTest').innerHTML = mainPrint("ls test");
 
 function mainPrint(line){
   //msg is meant for the final concat. string to be returned to the HTML file
@@ -23,7 +6,7 @@ function mainPrint(line){
   var lineArr = [];
   var msg;
 
-  lineArr = line.split("");
+  lineArr = line.split(" ");
   for (var i = 0; i < lineArr.length; i++) {
     if (lineArr[i] == "ls") {
       msg += lineArr[i].fontcolor("#39ff14");
